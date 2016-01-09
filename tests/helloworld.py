23e5fedc56458b405e81005876033ed0cb501a15
Juan Molero <juanmolerog@gmail.com>
2016-01-09 06:22:00 -0430

    Removed old tests and init placeholders

--- tests/helloworld.py
+++
@@ -1,8 +0,0 @@
-from pyHD44780 import PCF8574LCD
-
-lcd = PCF8574LCD(1,0x3f,16,2)
-
-lcd.setCursor(5,0)
-lcd.writeString("Hello")
-lcd.setCursor(5,1)
-lcd.writeString("World!")
--- tests/test.py
+++
@@ -1,5 +0,0 @@
-from pyHD44780 import PCF8574LCD
-
-lcd = PCF8574LCD(1,0x3f,16,2)
-
-lcd.writeString("Hello World!")
